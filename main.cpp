f15dccadf156f7e46ef3416203e0880f355827cd
Vadim <nikitin.vl@phystech.edu>
2021-04-01 19:47:07 +0300

    Организована безопасная работа с потоками

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int Is_equal(char a, char b);
 
 void Search_Substring(std::string main_string, std::string substring, std::vector<size_t>& Iterators, size_t left, size_t right);
 
-
+void parallel_search(std::string main_string, std::string substring, std::vector<size_t>& Iterators);
 
 int main()
 {
@@ -53,4 +53,26 @@ void Search_Substring(std::string main_string, std::string substring, std::vecto
         count = 0;
     }
     m_mutex.unlock();
+}
+
+void parallel_search(std::string main_string, std::string substring, std::vector<size_t>& Iterators)
+{
+    const std::size_t num_core = std::thread::hardware_concurrency();
+    int hardware_threads = num_core != 0 ? num_core : 4;
+    int thread_x_size = main_string.size() / hardware_threads;
+    int residue = main_string.size() % hardware_threads;
+    std::vector<std::thread> threads(hardware_threads);
+    double left = 0, right = thread_x_size;
+    for (size_t i = 0; i < threads.size(); i++)
+    {
+        threads[i] = std::thread(Search_Substring, main_string, substring, std::ref(Iterators), left, right);
+        left += thread_x_size;
+        right += thread_x_size;
+    }
+
+    if (residue != 0)
+    {
+        threads.push_back(std::thread(Search_Substring, main_string, substring, std::ref(Iterators), left, main_string.size()));
+    }
+    std::for_each(threads.begin(), threads.end(), std::mem_fn(&std::thread::join));
 }
