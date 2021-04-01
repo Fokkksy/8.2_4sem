3e27d3ae9858cb9007228a9c65456e81f47c2edd
Vadim <nikitin.vl@phystech.edu>
2021-04-01 19:33:41 +0300

    Начали писать программу

---
+++ main.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <vector>
+#include <random>
+#include <ctime>
+#include <thread>
+#include <mutex>
+#include <iterator>
+#include <string>
+#include <functional>
+
+int main()
+{
+	return 0;
+}
